567d0d47a3376400b2bd61184bd5c2dbbcdce2bd
ripytide <62516857+ripytide@users.noreply.github.com>
2021-05-13 13:40:06 +0100

    sql error

--- HandleNodePoll.php
+++ HandleNodePoll.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
   Query("UPDATE nodes SET last_polled=NOW() WHERE name=?", "s", $name);
   
 	//get relevant row from the database for the correct node
-	$node = Query("SELECT * FROM WHERE name=?", "s", $name);
+	$node = Query("SELECT * FROM nodes WHERE name=?", "s", $name);
   
   if ($node->num_rows === 0){
         $output["error_msg"] = "no tasks for that checklist";
