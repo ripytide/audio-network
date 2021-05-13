5e28db9a1b44b0326d844a471ebb4b81e96c65fc
ripytide <62516857+ripytide@users.noreply.github.com>
2021-05-13 11:21:34 +0100

    create hanle controller pell

--- HandleControllerPoll.php
+++ HandleControllerPoll.php
@@ -1,1 +1,20 @@
+<?php
 
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+	require_once("functions.php");
+  
+  $nodes = Connect()->query("SELECT * FROM nodes");
+  
+  //write nodes to nodes in output
+  if ($nodes->num_rows === 0){
+        $output["error_msg"] = "no nodes";
+    } else{
+          while($row = $nodes->fetch_assoc()){
+               $output["nodes"][] = $row;
+            }
+    }
+    
+  //send back info as json
+	echo(json_encode($output));
+}
+?>
