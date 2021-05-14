012409e0714b49f55d012f3cbaf62d1dd90c2dd7
ripytide <62516857+ripytide@users.noreply.github.com>
2021-05-14 15:13:28 +0100

    add audio_changed to controller post handler

--- HandleControllerPost.php
+++ HandleControllerPost.php
@@ -3,6 +3,6 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST"){
 	require_once("functions.php");
   
-	Query("UPDATE nodes SET audio_url=?,play_at=?,volume=? WHERE name=?", "ssss", $_POST["audio_url"], $_POST["play_at"], $_POST["volume"], $_POST["name"]);
+	Query("UPDATE nodes SET audio_url=?,play_at=?,volume=?,audio_changed=? WHERE name=?", "sssss", $_POST["audio_url"], $_POST["play_at"], $_POST["volume"], $_POST["audio_changed"], $_POST["name"]);
 }
 ?>
