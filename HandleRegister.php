044be7cadc02489c8b1a000fa4f50182460cf138
ripytide <62516857+ripytide@users.noreply.github.com>
2021-05-15 14:19:57 +0100

    Fix time going into name not last_polled

--- HandleRegister.php
+++ HandleRegister.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
 	require_once("functions.php");
 
 	//insert new node into database
-	Query("INSERT INTO nodes (name, last_polled, play_at, volume) values(?, ?, -1, 0)", "ss", time(), $_POST["name"]);
+	Query("INSERT INTO nodes (name, last_polled, play_at, volume) values(?, ?, -1, 0)", "ss", $_POST["name"], time());
 
 	//store name in session
 	session_start();
