254f1a35a980536e92d7dec0be7960a9f4f0458a
ripytide <james.forsterer@gmail.com>
2021-05-16 22:40:49 +0100

    Create a really cool (imo) time sync statistics page

--- timesync.js
+++ timesync.js
@@ -4,55 +4,124 @@
 //GetTime.php
 
 
-let number_of_pings = 100
-let time_between_pings = 500
-
-
-
-
+let number_of_pings = 10
+let time_between_pings = 1
 
 $(window).on("load", async function(){
-	let times = [];
-	let data = {
-		x: times,
+	let time_diffs = [];
+	let time_means = [];
+	let time_medians = [];
+	let time_modes = [];
+	let box_data = {
 		type: "box",
 		boxpoints: "all",
 		jitter: 0.2,
 		pointpos: -2,
+		name: ""
 	};
-	let layout = {};
-	Plotly.newPlot("box_plot", [data], layout);
+	let mean_data = {
+		line: {
+			width: 4
+		},
+		name: "Mean"
+	}
+	let median_data = {
+		line: {
+			width: 4
+		},
+		name: "Median"
+	}
+	let mode_data = {
+		line: {
+			width: 4
+		},
+		name: "Mode"
+	}
+	let histogram_data = {
+		type: "histogram",
+		xbins: {
+			size: 1
+		}
+	}
+	let box_layout = {
+		title: "Time Diffs",
+		xaxis: {
+			title: "Time/ms"
+		}
+	};
+	let line_layout = {
+		title: "Time Diffs Averages over (Time)",
+		xaxis: {
+			title: "Total Pings"
+		},
+		yaxis: {
+			title: "Time/ms"
+		}
+	};
+	let histogram_layout = {
+		title: "Time Diffs Histogram",
+		bargap: 0.05,
+	}
 
 	for (let i=0; i<number_of_pings; i++){
-		new_time_diff = get_time_diff("GetTime.php");
-		times.push(new_time_diff);
-		data.x = times;
-		layout.datarevision = i;
-		Plotly.react("box_plot", [data], layout);
-		Update_stats(times);
+		let new_diff = get_time_diff("GetTime.php");
+		time_diffs.push(new_diff)
+
+		box_data.x = time_diffs;
+
+		time_means.push(ss.mean(time_diffs));
+		mean_data.x = [...Array(i).keys()];
+		mean_data.y = time_means;
+
+		time_medians.push(ss.median(time_diffs));
+		median_data.x = [...Array(i).keys()];
+		median_data.y = time_medians;
+
+		time_modes.push(ss.mode(time_diffs));
+		mode_data.x = [...Array(i).keys()];
+		mode_data.y = time_modes;
+
+		histogram_data.x = time_diffs;
+
+		//these are necceccary for plotly.js to update its plots
+		box_layout.datarevision = i;
+		line_layout.datarevision = i;
+		histogram_layout.datarevision = i;
+
+		Plotly.react("box_plot", [box_data], box_layout);
+		Plotly.react("line_plot", [mean_data, median_data, mode_data], line_layout);
+		Plotly.react("histogram_plot", [histogram_data], histogram_layout);
+
+		//updates the table
+		Update_stats(time_diffs);
 
 		await new Promise(r => setTimeout(r, time_between_pings));
 	}
 
 });
 
-function Update_stats(times){
-	return 2;
+function Update_stats(times1){
+	document.getElementById("mean-1").innerHTML = ss.mean(times1).toFixed(2);
+	document.getElementById("median-1").innerHTML = ss.median(times1);
+	document.getElementById("stan-1").innerHTML = ss.standardDeviation(times1).toFixed(2);
+	document.getElementById("range-1").innerHTML = ss.max(times1) - ss.min(times1)
+	document.getElementById("iq-range-1").innerHTML = ss.interquartileRange(times1).toFixed(2);
+	
+	document.getElementById("ping_count").innerHTML = "Ping count: " + times1.length;
 }
-
 
 function get_time_diff(url){ //get the time difference in ms between current time and the time from a time api
 	time_before = Date.now()
 	let requested_time;
-	
+
 	$.ajax({
 		url,
 		dataType: "text",
 		async: false,
 		success: (time) => {
-			time_after = time
+			requested_time = time
 		}
 	});
 
-	return parseInt(time_after) - time_before
+	return parseInt(requested_time) - time_before
 }
