1eeb18d54acd2c116ae27e20fb8ca453356f9aeb
ripytide <62516857+ripytide@users.noreply.github.com>
2021-05-18 11:06:45 +0100

    make time poll node specific

--- HandleTimePoll.php
+++ HandleTimePoll.php
@@ -1,7 +1,7 @@
 <?php
 require_once("functions.php");
 
-$result = Query("SELECT change_at, changed FROM timesync WHERE name=?", "s", "all");
+$result = Query("SELECT change_at, changed FROM timesync WHERE name=?", "s", $_POST["name"]);
 
 $output = $result->fetch_assoc();
 
