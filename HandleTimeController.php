c822c64bb3e69f4af5547c27713dbd801bb9f698
ripytide <62516857+ripytide@users.noreply.github.com>
2021-05-18 11:10:44 +0100

    im fucking stupid callum

--- HandleTimeController.php
+++ HandleTimeController.php
@@ -2,6 +2,6 @@
 
 require_once("functions.php");
 
-Query("UPDATE timesync SET change_at=?, changed=true WHERE name=?", "is", time() + 10, "all");
+Query("UPDATE timesync SET change_at=?, changed=true", "i", time() + 10);
 
 ?>
